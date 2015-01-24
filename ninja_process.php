12b140dee7e201583217aa2feb31e1404bcfe35a
Casey LaRue <casey.larue@gmail.com>
2015-01-23 21:34:06 -0800

    changes to ninja gold game

--- ninja_process.php
+++ ninja_process.php
@@ -1,14 +1,55 @@
 <?php
 session_start();
 
-if(isset($_POST['music']))
+
+if(!isset($_SESSION['gold']))
 {
-	$_SESSION['music'] = $_POST['music'];
-}
-if(isset($_POST['color']))
+	$_SESSION['gold'] = 0;
+} 
+
+
+
+if(isset($_POST['building'])) 
 {
-	$_SESSION['color'] = $_POST['color'];
+	if($_POST['building'] == 'farm') 
+	{
+		$_SESSION['random'] = rand(10,20);
+		$_SESSION['gold'] = $_SESSION['gold'] + $_SESSION['random'];
+		$_SESSION['activity'][] = "you earned a farm and earned " . $_SESSION['random'] . " golds.";
+		header('location: ninja_gold.php');
+	}
+
+	if($_POST['building'] == 'cave') 
+	{
+		$_SESSION['random'] = rand(5,10);
+		$_SESSION['gold'] = $_SESSION['gold'] + $_SESSION['random'];
+		$_SESSION['activity'][] = "you earned a cave and earned " . $_SESSION['random'] . " golds.";
+		header('location: ninja_gold.php');
+	}
+
+	if($_POST['building'] == 'house') 
+	{
+		$_SESSION['random'] = rand(2,5);
+		$_SESSION['gold'] = $_SESSION['gold'] + $_SESSION['random'];
+		$_SESSION['activity'][] = "you earned a house and earned " . $_SESSION['random'] . " golds.";
+		header('location: ninja_gold.php');
+	}
+
+	if($_POST['building'] == 'casino') 
+	{
+		$_SESSION['random'] = rand(-50,50);
+		$_SESSION['gold'] = $_SESSION['gold'] + $_SESSION['random'];
+		header('location: ninja_gold.php');	
+	}
 }
 
+
+
+if(isset($_POST['reset'])) 
+{
+	session_destroy();
+	header('location: ninja_gold.php');
+	die();
+}
 
 ?>
